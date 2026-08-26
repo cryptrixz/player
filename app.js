023026b1e965a48f4721cb4c825197e8e80df5a2
cryptrixz <cttmpunk@gmail.com>
2026-08-26 14:45:37 -0400

    Refactor createOverlayWindow function in app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 function createOverlayWindow() {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-
     const win = new BrowserWindow({
         width: 480,
         height: 110,
@@ -20,17 +19,14 @@ function createOverlayWindow() {
             contextIsolation: true
         }
     });
-
     win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
     win.setAlwaysOnTop(true, 'screen-saver', 1);
     win.setIgnoreMouseEvents(true, { forward: true });
-
     win.loadFile(path.join(__dirname, 'overlay.html'));
 }
 
 app.whenReady().then(() => {
     createOverlayWindow();
-
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createOverlayWindow();
     });
